bc7b6ae0a88d8601ae80b6953f05c2e1891b77e4
CL2~PC17 <CL2~PC17@DESKTOP-6ER5G45>
2025-08-08 13:03:43 +0800

    bago

---
+++ uio/src/main/java/com/mycompany/uio/Uio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.uio;
+
+/**
+ *
+ * @author CL2~PC17
+ */
+public class Uio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
