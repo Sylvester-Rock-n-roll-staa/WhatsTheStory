3438a2d801d8b58e97f1ea9ce2dd6ecae15315d9
CL2~PC05 <CL2~PC05@DESKTOP-HAVSU57>
2025-08-15 12:06:24 +0800

    dknow

--- uio/src/main/java/com/mycompany/uio/Uio.java
+++ uio/src/main/java/com/mycompany/uio/Uio.java
@@ -11,6 +11,51 @@ package com.mycompany.uio;
 public class Uio {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Scanner player = new Scanner(System.in);
+        System.out.println("Enter First Number 1:");
+        int x = Integer.parseInt(player.nextLine());
+        System.out.println("Enter First Number 2:");
+        int y = Integer.parseInt(player.nextLine());
+        
+         
+        
+        System.out.println("Player1 1 = Rock  2 = Paper  3 = Scissor");
+        System.out.println("");
+        
+        System.out.println("Player 1: ");
+        int player1 = Integer.parseInt(player.nextLine());
+        System.out.println("");
+        int player2 = Integer.parseInt(player.nextLine());
+        System.out.println("Player 2: ");
+        if(player1 == 1 && player2 == 2){
+            System.out.print("Draw");
+        }
+        else if(player1 == 1 && player2 == 2){
+            System.out.print("Draw");
+        }
+        else if(player1 == 1 && player2 == 2){
+            System.out.print("Draw");
+        }
+        else if(player1 == 1 && player2 == 2){
+            System.out.print("Player 2 win!");
+        }
+        else if(player1 == 1 && player2 == 2){
+            System.out.print("Player 1 win!");
+        }
+        else if(player1 == 1 && player2 == 2){
+            System.out.print("Player 2 win!");
+        }
+        else if(player1 == 1 && player2 == 2){
+            System.out.print("Player 1 win!");
+        }
+        else if(player1 == 1 && player2 == 2){
+            System.out.print("Player 2 win!");
+        } 
+        else if(player1 == 1 && player2 == 2){
+            System.out.print("Player 1 win!");
+        }
+        else{
+            System.out.print("Invalid choice please select option");
+        }
     }
 }
