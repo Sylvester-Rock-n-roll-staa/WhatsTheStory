5aa617bcaeb8ae24eb82327b637a165f8d1ec3e5
USER <USER@DESKTOP-KKVJUCP>
2025-10-23 07:34:33 -0230

    lalala

---
+++ uio/src/main/java/com/mycompany/uio/HashMap.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.uio;
+import java.util.HashMap;
+/**
+ *
+ * @author USER
+ */
+public class HashMap {
+    public static void main(String[] args) {
+	 // Create a HashMap object called capitalCities
+    HashMap<String, String> capitalCities = new HashMap<String, String>();
+
+    // Add keys and values (Country, City)
+    capitalCities.put("England", "London");
+    capitalCities.put("Germany", "Berlin");
+    capitalCities.put("Norway", "Oslo");
+    capitalCities.put("USA", "Washington DC");
+    System.out.println(capitalCities);
+	}
+}
